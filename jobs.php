0bfe13d6a9ecde8d3a1d89bdce5fd8255a23805b
Juan <kmirojo@gmail.com>
2018-10-25 22:07:25 -0500

    ActualizaciÂón de la Clase Jobs

---
+++ jobs.php
@@ -0,0 +1,12 @@
+<?php
+
+class Job {
+    public $title;
+    public $description;
+    public $visible;
+    public $months;
+}
+
+$job1 = new Job();
+$job1->title = 'PHP Developer';
+$job1->description = 'This is an awesome job!!!';
